5d2a19d7fa39017f33d37ff2520e9848022c2c07
Cristian Eduardo <cristian.santos0308@gmail.com>
2024-04-11 22:06:14 -0300

    Criando hook para buscar posts

--- src/pages/home/Home.js
+++ src/pages/home/Home.js
@@ -26,7 +26,7 @@ const Home = () => {
         {posts && posts.length === 0 && (
           <div className={styles.noposts}>
             <p>Não foram encontrados posts</p>
-            <Link to="/posts/create" className='btn'>Criar primeiro post</Link>
+            <Link to="/posts/create" className="btn">Criar primeiro post</Link>
           </div>
         )}
       </div>

369443555bf8d98a792d986da210ab0c1274f13c
Cristian Eduardo <cristian.santos0308@gmail.com>
2024-04-11 21:09:20 -0300

    Vaidando dados

--- src/pages/createPost/CreatePost.js
+++ src/pages/createPost/CreatePost.js
@@ -16,27 +16,42 @@ const CreatePost = () => {
   const {user} = useAuthValue();
 
   const {insertDocument, response} = useInsertDocument("posts");
+  
+  const navigate = useNavigate(); 
 
   const handleSubmit = (e) => {
     e.preventDefault();
     setFormError("");
 
     //validate image URL
+    try {
+       new URL(image);
+    } catch (error) {
+        setFormError("A imagem precisa ser uma URL");
+    }
 
     // criar array de tags 
+    const tagsArray = tags.split(",").map((tag) => tag.trim().toLowerCase());
 
     // checar valores
+    if(!title || !image || !tags || !body) {
+      setFormError("Por favor, preencha todos os campos!");
+    }
+
+    if (formError) return;
+
 
     insertDocument({
       title,
       image,
-      tags,
+      tags : tagsArray,
       body,
       uid: user.uid,
       createdBy: user.displayName,
     });
 
     // redirect  Home Page
+    navigate("/")
   };
 
   return (
@@ -94,6 +109,7 @@ const CreatePost = () => {
           </button>
         )}
         {response.error && <p className="error">{response.error}</p>}
+        {formError && <p className="error">{formError}</p>}
       </form>
     </div>
   );

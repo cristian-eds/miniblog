981e46014d65e885f6a4924c481cc499f1f0c1d9
Cristian Eduardo <cristian.santos0308@gmail.com>
2024-09-11 15:48:53 -0300

    Criado hook useFetchDocument

--- src/pages/post/Post.js
+++ src/pages/post/Post.js
@@ -3,14 +3,25 @@ import styles from './Post.module.css';
 
 //hooks
 import { useParams } from 'react-router-dom';
+import { useFetchDocument } from '../../hooks/useFetchDocument';
 
 const Post = () => {
-    const {id} = useParams();
-  return (
-    <div>
-        <h1>Post {id}</h1>
-    </div>
-  )
+    const { id } = useParams();
+
+    const { document: post, loading } = useFetchDocument("posts", id);
+
+    return (
+        <div>
+            {loading &&  <p>Carregando Post ...</p> }
+            {post && (
+                <>
+                    <h1>
+                        {post.title}
+                    </h1>
+                </>
+            )}
+        </div>
+    )
 }
 
 export default Post
